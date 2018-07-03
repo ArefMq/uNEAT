debc4ad6cc413af20e66d5894d6ef0dfeaf42d08
Aref Moqadam Mehr <arefmq@cafebazaar.ir>
2018-07-03 13:22:25 +0430

    add licence printing and do some cleanups

--- train.py
+++ train.py
@@ -4,24 +4,19 @@ from dataset import dataset
 
 
 if __name__ == '__main__':
-    gn = Genetics(initial_population=300, max_population=9000)
+    print('running uNEAT-T1 (Test)')
+    print('micro-(Neuro-Evolution of Augmenting Topology) Framework -- Version T1')
+    print('Licenced under MIT')
 
+    gn = Genetics(initial_population=300, max_population=9000)
     best = gn.get_best(max_iter=40000, optimal_fitness=0.02)
-    print(best)
 
     print 'validating::'
-    n3 = best.Genes
-    print('Fitness:    %0.4f' % fitness(n3, dataset))
-    print('error:      %0.4f' % error(n3, dataset))
-    print('regularity: %0.4f' % regularity(n3, dataset))
+    print('Fitness:    %0.4f' % fitness(best.Genes, dataset))
+    print('error:      %0.4f' % error(best.Genes, dataset))
+    print('regularity: %0.4f' % regularity(best.Genes, dataset))
 
-    print '0 0', binary_classify(n3, [0, 0])
-    print '0 1', binary_classify(n3, [0, 1])
-    print '1 0', binary_classify(n3, [1, 0])
-    print '1 1', binary_classify(n3, [1, 1])
-
-    # try:
-    #     best = gn.get_best(max_iter=100000)
-    #     print(best)
-    # except Exception as ex:
-    #     print('\n\n%s\n' % ex.message)
+    print '0 0', binary_classify(best.Genes, [0, 0])
+    print '0 1', binary_classify(best.Genes, [0, 1])
+    print '1 0', binary_classify(best.Genes, [1, 0])
+    print '1 1', binary_classify(best.Genes, [1, 1])
